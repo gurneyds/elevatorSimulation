9e49edc75e86768828761a1263d974d79d98ed0c
David Gurney <gurneyds@yahoo.com>
2016-08-25 04:55:45 -0600

    Setup

---
+++ src/main/java/org/kuali/careers/elevator/ElevatorSimulator.java
@@ -0,0 +1,10 @@
+package org.kuali.careers.elevator;
+
+/**
+ * Created by gurneyds on 8/25/16.
+ */
+public class ElevatorSimulator {
+	public static void main(String args[]) {
+		System.out.println("Welcome to the elevator simulator");
+	}
+}
---
+++ src/test/java/org/kuali/careers/elevator/ElevatorSimulatorTest.java
@@ -0,0 +1,7 @@
+package org.kuali.careers.elevator;
+
+/**
+ * Created by gurneyds on 8/25/16.
+ */
+public class ElevatorSimulatorTest {
+}
